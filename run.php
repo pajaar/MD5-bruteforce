17948c83ad4bd943cab88896df175fa691e86db3
pajaR_19 <53967072+pajaar@users.noreply.github.com>
2020-09-26 12:58:26 +0700

    Update run.php

--- run.php
+++ run.php
@@ -3,7 +3,7 @@
 // github.com/pajaar
 
 $pass = "578b996f48ab4bcad40f9ee455581e5f";
-$list = "list.txt";
+$list = "wordlist.txt";
 
 $lol = explode("\r\n", file_get_contents($list));
 foreach($lol as $res){
